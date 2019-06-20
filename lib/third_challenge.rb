f048219c48d9cd1f75de4ca97a6e21ca1845ab13
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-06-20 14:11:20 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,30 +2,30 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague", :age => "53"
+        name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        :name => "Lady Montague", :age => "54"
+        name: "Lady Montague", age: "54"
         },
       :hero => {
-        :name => "Romeo", :age => "15", :status => "alive"
+        name: "Romeo", age: "15", status:"alive"
         },
-      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"}, 
-        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, 
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
         ]
    }, 
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet", :age => "50"
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        :name => "Lady Capulet", :age => "51"
+        name:"Lady Capulet", name: "51"
         },
       :heroine => {
-        :name => "Juliet", :age => "15", :status => "alive"
+        name: "Juliet", name: "15", status: "alive"
         },
-      :heroine_friends => [:name => "Steven", :age => "30", :attitude => "confused"},
-        {:name => "Nurse", :age => "44", :attitude => "worried"}
+      :heroine_friends => [name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude:"worried"}
         ]
    }
   }
