7a27b599b81b30121b6e8d3660b18b66e64f796c
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-04 15:58:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,4 +1,6 @@
-def first_challenge
-   epic_tragedy = {":montague => {}, :capulet => {}"}
-   
-end
+'spec_helper'
+'../lib/first_challenge.rb'
+
+describe "first_challenge"
+  it "sets a variable, epic_tragedy, equal to a hash containing keys of family names and values of empty hashes"
+  expect
