6b48ac3a1a1b1fa7be933e3cfe5fe041559752c0
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-20 03:59:15 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,8 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+    },
     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       #your key/value pairs here!
       }}
 
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,29 +2,48 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
         #your key/value pairs here
         },
       :hero => {
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => {
+        "Benvolio" => {
+        :age => "17",
+        :attitude => "worried",
+        :name => "Benvolio",
+      }
+   },
    :capulet => {
       :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
         #your key/value pairs here
         },
       :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
