67c4977008badfe747fbf3c20fe43c94fd174a6e
Matt Plichta <bigcatplichta@gmail.com>
2019-08-22 01:43:31 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,10 +20,11 @@ def bonus
    }
   }
 
-  #code your solution here:
+ epic_tragedy[:montague][:hero][:status] = "dead as fuck"
+ epic_tragedy[:capulet][:heroine][:status] = "dead as fuck"
 
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
+  return epic_tragedy
 end
