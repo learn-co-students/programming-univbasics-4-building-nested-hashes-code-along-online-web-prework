1a067582dc3033a3563500dd7360a307a12aca74
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-23 05:01:36 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,16 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = movie title  
+   '{ ROmeo + Juliet 
+   
+   cast: "Leonardo DiCaprio" 
+   character => "ROmeo"
+   "ROmeo's attrilbutes"
+   => romatic,kisser,boyfriend
+     
+  cast:"Claire Danes" 
+     character => ["Juliet"]
+     
+     ["Juliet"] attrilbutes 
+     => princess, drink poision 
+   }'
 end
