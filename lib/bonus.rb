d3d6021a9f4db282815c66af0bd174e4aa07ac59
William <teststuffreally@gmail.com>
2019-10-07 14:16:32 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -3,26 +3,6 @@ def bonus
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Steven", age: "30", attitude: "confused"}
-      ]
-   }, 
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
-          {name: "Nurse", age: "44", attitude: "worried"}
-      ]
-   },
-  }
-
-    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "dead"},
       :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
@@ -32,14 +12,15 @@ def bonus
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "dead"},
       :heroine_friends => [
           {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
-   }
-  }
+    
 
+  
+  
 
   
 
