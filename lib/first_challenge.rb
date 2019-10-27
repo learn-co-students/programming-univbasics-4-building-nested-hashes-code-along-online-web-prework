02ad9e8c34186180056a58ee3f0bb89af64228e9
Sean Good <tree662@protonmail.com>
2019-10-27 03:31:55 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -26,6 +26,32 @@ def first_challenge
          }
       ]
      },
-      :capulet =>
+      :capulet => {
+        :patriarch => {
+          :name => "Lord Capulet",
+          :age => "50"
+        },
+        :matriarch => {
+          :name => "Lady Capulet",
+          :age => "50"
+        },
+        :heroine => {
+          :name => "Juliet",
+          :age => "15",
+          :status => "alive"
+        },
+        :heroine_friends => [
+          {
+            :name => "Steven",
+            :age => "30",
+            :attitude => "confused"
+          },
+          {
+            :name => "Nurse",
+            :age => "44",
+            :attitude => "worried"
+          }
+        ]
+      }
    }
 end
