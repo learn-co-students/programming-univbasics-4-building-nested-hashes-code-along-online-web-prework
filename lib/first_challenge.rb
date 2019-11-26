4e7c8e5bf940d927d98ebb0c94388c87c8077e65
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-26 04:27:45 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,28 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+   :montague =>{"Lord Montague"
+    
+   :patriarch =>{
+   :matriiarch =>"Lady Montague",
+   :hero => "Romeo" ,
+   :hero_friends => "Friar Lawrence"
+
+  
+
+     
+     
+     
+     
+   }
+   :capulet =>{"Capulet"
+   :patriarch =>{
+   :matriiarch =>"Lady Capulet",
+   :heroine => "Juliet",
+   :heroine_friends => "Friar Lawrence"
+   }
+
+
+
+
 end
+
